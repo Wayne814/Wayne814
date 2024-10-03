7c173d36765f17ade5d9cf1b029d8027df6d2508
Wayne814 <griffins2k@gmail.com>
2024-10-03 20:07:19 +0300

    push the project

---
+++ Loan.cpp
@@ -0,0 +1,23 @@
+/*programme based on loan
+author:Raymond reddington
+reg:CT100/G/22430/24*/
+#include <stdio.h>
+int main() 
+{
+	int age;
+	float salary;
+	//prompt user to enter the age
+	printf("enter the age:");
+	scanf("%d",&age);
+	//prompt the user to enter the salary
+	printf("enter the salary:");
+	scanf("%f",&salary);
+	if (age>=21,"salary>=21000"){
+		printf("congratulations");
+		}
+		else
+		{
+			printf("unfortunately");
+		}
+         return 0;
+}
